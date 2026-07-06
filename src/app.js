b054953db367def0e2d235efcd76868c4d6a5728
Joyeeta Das <joyeetadas597@gmail.com>
2026-07-06 11:24:03 +0530

    post creation done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const authRouter = require("./routers/auth.router")
+const postRouter = require("./routers/post.router")
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRouter)
+app.use("/api/posts", postRouter)
 
 
 module.exports = app
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -9,9 +9,13 @@ const postSchema = new mongoose.Schema({
         type:String,
         require:[true,"image url is require for the post"]
     },
-    user:{
-        type:mongoose.Schema.type.objectId,
-        ref:"users",
-        require:[true,"user id is require for creating a post"]
-    }
+    user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: [true, "User ID is required for creating a post"]
+}
 })
+
+const postModel = mongoose.model("posts", postSchema)
+
+module.exports = postModel
